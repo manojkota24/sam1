d49870f10d8320a5dfe707fb910ebfaf4038c2b8
manojkota24 <97742963+manojkota24@users.noreply.github.com>
2022-01-16 23:07:19 +0530

    Update hw.py

--- hw.py
+++ hw.py
@@ -3,4 +3,5 @@ app=Flask(__name__)
 @app.route('/')
 def hello():
     return 'hello this is manoj'
-app.run(host='127.0.0.1',port=8080,debug=True)
+if __name__== '__main__':
+    app.run(host='127.0.0.1', port=8080 , debug=True)
